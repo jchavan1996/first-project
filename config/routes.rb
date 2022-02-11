8c1b4269eefc437b12b19596f3a0b821b792ecdd
jchavan1996 <jitendrachavan.2011@gmail.com>
2022-02-11 17:32:19 +0530

    adding navbar and login link registration link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   root 'visitor#index'
+  get 'about', to:'about#index'
+  get 'contact', to:'contact#index'
+  
   # get 'visitor/index'
+  # get 'contact/index'
+  # get 'about/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
